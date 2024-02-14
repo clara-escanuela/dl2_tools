f9ceebba42e6aea5b2f9ee61f7ac9e4c3b2ece17
gschwefer <georg.schwefer@mpi-hd.mpg.de>
2024-02-14 14:02:10 +0100

    make it possible to read hess sims

--- src/dl2_tools/handler/sim_datasets.py
+++ src/dl2_tools/handler/sim_datasets.py
@@ -9,14 +9,14 @@ from astropy.coordinates import AltAz
 import astropy.units as u
 from ctapipe.io import TableLoader
 from copy import copy
-
+import uproot
 
 class SimDataset(ABC):
     """
     Base class for handling dl2 datasets
     """
 
-    def __init__(self, loader, reco_energy_name, gh_score_name, obs_time=1 * u.s):
+    def __init__(self, path,reco_energy_name, hess_root=False,obs_time=1 * u.s,siminfo_dict=None):
         """
         Constructor of the Base Class. Initializes Datset from ctapipe TableLoader
 
@@ -26,17 +26,34 @@ class SimDataset(ABC):
             ctapipe TableLoader object conatining dl2 data
         reco_energy_name : string
             Prefix of the reconstructed energy columns in the table
-        gh_score_name : string
-            Prefix of the g/h separation score columns
         obs_time : u.Quantity time, optional
             Observation time of the dataset. Needed to make the weights.
             By default 1*u.s, this makes it easy to calculate weights in 1/s
         """
         self.obs_time = obs_time
-        self.events = QTable(loader.read_subarray_events())
-
+        self.hess_root=hess_root
+        if not self.hess_root:
+            self.loader = TableLoader(
+            path,
+            load_true_images=True,
+            load_dl1_images=False,
+            load_dl1_parameters=True,
+            load_simulated=True,
+            load_dl2=True,
+        )
+            self.events=QTable(self.loader.read_subarray_events())
+        else:
+            self.events=QTable()
+            file=uproot.open(path)
+            self.par_tree = file["ParTree_Postselect;1"]
+            self.events["true_energy"]=u.Quantity(np.array(self.par_tree["MCTrueEnergy"].array()),u.TeV)
+            self.events["true_az"]=u.Quantity(np.array(self.par_tree["MCTrueAzimuth"].array()),u.deg)
+            self.events["true_alt"]=u.Quantity(np.array(self.par_tree["MCTrueAlt"].array()),u.deg)
+            self.events[f"{reco_energy_name}Energy"]=u.Quantity(np.array(self.par_tree[f"{reco_energy_name}Energy"].array()),u.TeV)
+            self.events["mc_event_weight"]=np.array(self.par_tree["MCEventWeight"].array())
+            
         self._set_reco_energy_column(reco_energy_name)
-        self._set_gh_score_column(gh_score_name)
+        
 
     @abstractmethod
     def _set_simulation_info(self, sim_config, obs_info):
@@ -87,6 +104,30 @@ class SimDataset(ABC):
             alt=obs_info["subarray_pointing_lat"][0],
         )
 
+    @abstractmethod
+    def _set_simulation_info_hess_root(self, path, siminfo_dict):
+
+
+        # If we later want to compare observations at different zeniths, we better make
+        # the pointing direction a class attribute
+        self.pointing_direction = AltAz(
+            az=siminfo_dict["pointing_az"]*u.deg,
+            alt=siminfo_dict["pointing_alt"]*u.deg,
+        )
+
+        file=uproot.open(path)
+        sim_energy_histo=file.get("ThrownEnergy;1")
+
+        self.simulation_info = SimulatedEventsInfo(
+            n_showers=sim_energy_histo.values().sum(),
+            max_impact=siminfo_dict["max_impact"]*u.m,
+            viewcone_min=siminfo_dict["viewcone_min"]*u.deg,
+            viewcone_max=siminfo_dict["viewcone_max"]*u.deg,
+            energy_min=sim_energy_histo.axis().edges()[0]*u.TeV,
+            energy_max=sim_energy_histo.axis().edges()[-1]*u.TeV,
+            spectral_index=siminfo_dict["sim_spectral_index"],
+        )
+
     def _set_simulation_weights(self):
         """
         Generates columns "weight" and "weight_rate" based on the simulated
@@ -97,34 +138,60 @@ class SimDataset(ABC):
         self.events["weight_rate"] = np.repeat(
             u.Quantity(1 / self.obs_time), len(self.events)
         )
+        if "true_aweight" in self.events.keys():
+            if np.all(~np.isnan(self.events["true_aweight"])):
+                self.events["weight"]*=(self.events["true_aweight"]/(np.pi * self.simulation_info.max_impact ** 2))
+                self.events["weight_rate"]*=(self.events["true_aweight"]/(np.pi * self.simulation_info.max_impact ** 2))
+        if "mc_event_weight" in self.events.keys():
+            self.events["weight"]*=self.events["mc_event_weight"]
+            self.events["weight_rate"]*=self.events["mc_event_weight"]
 
     def _set_reco_energy_column(self, reco_energy_name):
-        reco_energy_string = f"{reco_energy_name}_energy"
+        if self.hess_root:
+            reco_energy_string = f"{reco_energy_name}Energy"
+        else:
+            reco_energy_string = f"{reco_energy_name}_energy"
         if reco_energy_name != "ImPACTReconstructor":
             reco_energy_valid_string = f"{reco_energy_name}_is_valid"
         else:
             reco_energy_valid_string = f"{reco_energy_name}_is_valid_1"
         self.events["reco_energy"] = self.events[reco_energy_string]
-        self.events["reco_energy_is_valid"] = self.events[reco_energy_valid_string]
+        if not self.hess_root:
+            self.events["reco_energy_is_valid"] = self.events[reco_energy_valid_string]
+        else:
+            self.events["reco_energy_is_valid"] = np.repeat(True,len(self.events))
+        self.events["rel_E_diff"]=((self.events["reco_energy"]-self.events["true_energy"])/self.events["true_energy"]).to_value(u.one)
 
+    @abstractmethod
     def _set_gh_score_column(self, gh_score_name):
-        gh_score_string = f"{gh_score_name}_prediction"
-        gh_score_valid_string = f"{gh_score_name}_is_valid"
-        self.events["gh_score"] = self.events[gh_score_string]
-        self.events["gh_score_is_valid"] = self.events[gh_score_valid_string]
+        if gh_score_name is not None:
+            gh_score_string = f"{gh_score_name}_prediction"
+            gh_score_valid_string = f"{gh_score_name}_is_valid"
+        
+            self.events["gh_score"] = self.events[gh_score_string]
+            self.events["gh_score_is_valid"] = self.events[gh_score_valid_string]
 
     @abstractmethod
     def _set_geometry_columns(self, geometry_reco_name):
-        self.events["reco_az"] = self.events[f"{geometry_reco_name}_az"]
-        self.events["reco_alt"] = self.events[f"{geometry_reco_name}_alt"]
-        if geometry_reco_name != "ImPACTReconstructor":
-            self.events["geometry_reco_is_valid"] = self.events[
-                f"{geometry_reco_name}_is_valid"
-            ]
+        if self.hess_root:
+            self.events["reco_az"] = self.events[f"{geometry_reco_name}AzEvent"]
+            self.events["reco_alt"] = self.events[f"{geometry_reco_name}AltEvent"]
         else:
-            self.events["geometry_reco_is_valid"] = self.events[
-                f"{geometry_reco_name}_is_valid_1"
-            ]
+            self.events["reco_az"] = self.events[f"{geometry_reco_name}_az"]
+            self.events["reco_alt"] = self.events[f"{geometry_reco_name}_alt"]
+
+        if not self.hess_root:
+            if geometry_reco_name != "ImPACTReconstructor":
+                self.events["geometry_reco_is_valid"] = self.events[
+                    f"{geometry_reco_name}_is_valid"
+                ]
+            else:
+                self.events["geometry_reco_is_valid"] = self.events[
+                    f"{geometry_reco_name}_is_valid_1"
+                ]
+        else:
+            self.events["geometry_reco_is_valid"]=np.repeat(True,len(self.events))
+
 
         self.events["reco_source_fov_offset"] = angular_separation(
             self.pointing_direction.az,
@@ -141,17 +208,17 @@ class SimDataset(ABC):
             ),
         )
 
-    @classmethod
-    def from_path(cls, path, reco_energy_name, gh_score_name, obs_time=1 * u.s):
-        loader = TableLoader(
-            path,
-            load_true_images=True,
-            load_dl1_images=False,
-            load_dl1_parameters=True,
-            load_simulated=True,
-            load_dl2=True,
-        )
-        return cls(loader, reco_energy_name, gh_score_name, obs_time)
+    #@classmethod
+    #def from_path(cls, path, reco_energy_name, obs_time=1 * u.s):
+    #    loader = TableLoader(
+    #        path,
+    #        load_true_images=True,
+    #        load_dl1_images=False,
+    #        load_dl1_parameters=True,
+    #        load_simulated=True,
+    #        load_dl2=True,
+    #    )
+    #    return cls( reco_energy_name,loader, obs_time)
 
     def reweight_to(self, new_spectrum):
         reweight_factors = calculate_event_weights(
@@ -224,11 +291,15 @@ class SimDataset(ABC):
 
 
 class DiffuseDataset(SimDataset):
-    def __init__(self, loader, reco_energy_name, gh_score_name, obs_time=1 * u.s):
-        super().__init__(loader, reco_energy_name, gh_score_name, obs_time)
-        sim_config = QTable(loader.read_simulation_configuration())
-        obs_info = QTable(loader.read_observation_information())
-        self._set_simulation_info(sim_config, obs_info)
+    def __init__(self, path, reco_energy_name,hess_root=False, obs_time=1 * u.s,siminfo_dict=None):
+        super().__init__(path, reco_energy_name, hess_root, obs_time,siminfo_dict)
+        if not self.hess_root:
+            sim_config = QTable(self.loader.read_simulation_configuration())
+            obs_info = QTable(self.loader.read_observation_information())
+            self._set_simulation_info(sim_config, obs_info)
+        else:
+            assert siminfo_dict is not None
+            self._set_simulation_info_hess_root(path,siminfo_dict)
         self._set_simulation_weights()
 
     def _set_simulation_info(self, sim_config, obs_info):
@@ -244,7 +315,8 @@ class DiffuseDataset(SimDataset):
         self.simulation_info = SimulatedEventsInfo(
             n_showers=np.sum(sim_config["n_showers"] * sim_config["shower_reuse"]),
             max_impact=sim_config["max_scatter_range"][0],
-            viewcone=sim_config["max_viewcone_radius"][0],
+            viewcone_min=0*u.deg,
+            viewcone_max=sim_config["max_viewcone_radius"][0],
             energy_min=sim_config["energy_range_min"][0],
             energy_max=sim_config["energy_range_max"][0],
             spectral_index=sim_config["spectral_index"][0],
@@ -254,15 +326,31 @@ class DiffuseDataset(SimDataset):
             az=obs_info["subarray_pointing_lon"][0],
             alt=obs_info["subarray_pointing_lat"][0],
         )
-        self.radius = self.simulation_info.viewcone
+        self.radius = self.simulation_info.viewcone_max
+
+    def _set_simulation_info_hess_root(self,path,siminfo_dict):
+
+        super()._set_simulation_info_hess_root(path,siminfo_dict)
+
+        self.radius=siminfo_dict["viewcone_max"]*u.deg
+        
+        self.source_position = AltAz(
+            az=np.max(self.events["true_az"]), alt=np.max(self.events["true_alt"])
+        )
 
 
 class PointSourceDataset(SimDataset):
-    def __init__(self, loader, reco_energy_name, gh_score_name, obs_time=1 * u.s):
-        super().__init__(loader, reco_energy_name, gh_score_name, obs_time)
-        sim_config = QTable(loader.read_simulation_configuration())
-        obs_info = QTable(loader.read_observation_information())
-        self._set_simulation_info(sim_config, obs_info)
+    def __init__(self, path,reco_energy_name,hess_root=False, obs_time=1 * u.s,siminfo_dict=None):
+        super().__init__(path,reco_energy_name,hess_root, obs_time,siminfo_dict)
+
+        if not self.hess_root:
+            sim_config = QTable(self.loader.read_simulation_configuration())
+            obs_info = QTable(self.loader.read_observation_information())
+            self._set_simulation_info(sim_config, obs_info)
+        else:
+            assert siminfo_dict is not None
+            self._set_simulation_info_hess_root(path,siminfo_dict)
+
         self._set_simulation_weights()
 
     def _set_simulation_info(self, sim_config, obs_info):
@@ -303,26 +391,46 @@ class PointSourceDataset(SimDataset):
         self.simulation_info = SimulatedEventsInfo(
             n_showers=np.sum(sim_config["n_showers"] * sim_config["shower_reuse"]),
             max_impact=sim_config["max_scatter_range"][0],
-            viewcone=sim_config["max_viewcone_radius"][0],
+            viewcone_min=0*u.deg,
+            viewcone_max=sim_config["max_viewcone_radius"][0],
             energy_min=sim_config["energy_range_min"][0],
             energy_max=sim_config["energy_range_max"][0],
             spectral_index=sim_config["spectral_index"][0],
         )
+    def _set_simulation_info_hess_root(self,path,siminfo_dict):
 
+        super()._set_simulation_info_hess_root(path,siminfo_dict)
+
+        self.offset=siminfo_dict["offset"]*u.deg
+        
+        self.source_position = AltAz(
+            az=np.max(self.events["true_az"]), alt=np.max(self.events["true_alt"])
+        )
 
 class SignalSet(SimDataset):
     def __init__(
         self,
-        loader,
+        path,
         reco_energy_name,
         geometry_reco_name,
-        gh_score_name,
+        gh_score_name=None,
+        hess_root=False,
         obs_time=1 * u.s,
+        siminfo_dict=None
     ):
-        super().__init__(loader, reco_energy_name, gh_score_name, obs_time)
+        super().__init__(path,reco_energy_name,hess_root, obs_time,siminfo_dict)
+        if self.hess_root:
+            self.events[f"{geometry_reco_name}AzEvent"]=u.Quantity(np.array(self.par_tree[f"{geometry_reco_name}AzEvent"].array()),u.deg)
+            self.events[f"{geometry_reco_name}AltEvent"]=u.Quantity(np.array(self.par_tree[f"{geometry_reco_name}AltEvent"].array()),u.deg)
+        self._set_gh_score_column(gh_score_name)
         self._set_geometry_columns(geometry_reco_name)
         self._set_valid_column()
         self.set_cuts(None)
+    
+    def _set_gh_score_column(self, gh_score_name):
+        self.events["gh_score"] = np.ones(len(self.events))
+        self.events["gh_score_is_valid"] = np.ones(len(self.events)).astype(bool)
+        super()._set_gh_score_column(gh_score_name)
 
     def _set_geometry_columns(self, geometry_reco_name):
         super()._set_geometry_columns(geometry_reco_name)
@@ -340,76 +448,102 @@ class SignalSet(SimDataset):
             self.events["true_alt"],
         )
 
-    @classmethod
-    def from_path(
-        cls, path, reco_energy_name, geometry_name, gh_score_name, obs_time=1 * u.s
-    ):
-        loader = TableLoader(
-            path,
-            load_true_images=True,
-            load_dl1_images=False,
-            load_dl1_parameters=True,
-            load_simulated=True,
-            load_dl2=True,
-        )
-        return cls(loader, reco_energy_name, geometry_name, gh_score_name, obs_time)
+    #@classmethod
+    #def from_path(
+    #    cls, path, reco_energy_name, geometry_name, gh_score_name=None, obs_time=1 * u.s
+    #):
+    #    loader = TableLoader(
+    #        path,
+    #        load_true_images=True,
+    #        load_dl1_images=False,
+    #        load_dl1_parameters=True,
+    #        load_simulated=True,
+    #        load_dl2=True,
+    #    )
+    #    return cls(reco_energy_name, geometry_name, gh_score_name,loader, obs_time)
 
+    #@classmethod
+    #def from_hess_event_tree(
+    #    cls, path, reco_energy_name, geometry_name, gh_score_name=None, obs_time=1 * u.s
+    #):
+    #    events=QTable()
+    #    file=uproot.open(path)
+    #    par_tree = file["ParTree_^Postselect;1"]
+    #    events["true_energy"]=u.Quantity(np.array(par_tree["MCTrueEnergy"].array()),u.TeV)
+    #    events["true_az"]=u.Quantity(np.array(par_tree["MCTrueAzimuth"].array()),u.TeV)
+    #    events["true_alt"]=u.Quantity(np.array(par_tree["MCTrueAlt"].array()),u.TeV)
+    #    return cls(events,reco_energy_name, geometry_name, gh_score_name,True, obs_time)
 
 class PointSourceSignalSet(SignalSet, PointSourceDataset):
     def __init__(
         self,
-        loader,
+        path,
         reco_energy_name,
         geometry_reco_name,
-        gh_score_name,
+        gh_score_name=None,
+        hess_root=False,
         obs_time=1 * u.s,
+        siminfo_dict=None
     ):
         super().__init__(
-            loader, reco_energy_name, geometry_reco_name, gh_score_name, obs_time
+            path, reco_energy_name, geometry_reco_name, gh_score_name,hess_root, obs_time,siminfo_dict
         )
 
 
 class DiffuseSignalSet(SignalSet, DiffuseDataset):
     def __init__(
         self,
-        loader,
+        path,
         reco_energy_name,
         geometry_reco_name,
-        gh_score_name,
+        gh_score_name=None,
+        hess_root=False,
         obs_time=1 * u.s,
+        siminfo_dict=None
     ):
         super().__init__(
-            loader, reco_energy_name, geometry_reco_name, gh_score_name, obs_time
+            path, reco_energy_name, geometry_reco_name, gh_score_name,hess_root, obs_time, siminfo_dict
         )
 
 
 class BackgroundSet(DiffuseDataset):
     def __init__(
         self,
-        loader,
+        path,
         reco_energy_name,
         geometry_reco_name,
-        gh_score_name,
+        gh_score_name=None,
+        hess_root=False,
         obs_time=1 * u.s,
+        siminfo_dict=None
     ):
-        super().__init__(loader, reco_energy_name, gh_score_name, obs_time)
+        super().__init__(path,reco_energy_name,hess_root, obs_time,siminfo_dict)
+        if self.hess_root:
+            self.events[f"{geometry_reco_name}AzEvent"]=u.Quantity(np.array(self.par_tree[f"{geometry_reco_name}AzEvent"].array()),u.deg)
+            self.events[f"{geometry_reco_name}AltEvent"]=u.Quantity(np.array(self.par_tree[f"{geometry_reco_name}AltEvent"].array()),u.deg)
+        self._set_gh_score_column(gh_score_name)
         self._set_geometry_columns(geometry_reco_name)
         self._set_valid_column()
         self.set_cuts(None)
+    
+    def _set_gh_score_column(self, gh_score_name):
+        self.events["gh_score"] = np.zeros(len(self.events))
+        self.events["gh_score_is_valid"] = np.ones(len(self.events)).astype(bool)
+        super()._set_gh_score_column(gh_score_name)
 
     def _set_geometry_columns(self, geometry_reco_name):
         super()._set_geometry_columns(geometry_reco_name)
 
-    @classmethod
-    def from_path(
-        cls, path, reco_energy_name, geometry_name, gh_score_name, obs_time=1 * u.s
-    ):
-        loader = TableLoader(
-            path,
-            load_true_images=True,
-            load_dl1_images=False,
-            load_dl1_parameters=True,
-            load_simulated=True,
-            load_dl2=True,
-        )
-        return cls(loader, reco_energy_name, geometry_name, gh_score_name, obs_time)
+    #@classmethod
+    #def from_path(
+    #    cls, path, reco_energy_name, geometry_name, gh_score_name=None, obs_time=1 * u.s
+    #):
+    #    loader = TableLoader(
+    #        path,
+    #        load_true_images=True,
+    #        load_dl1_images=False,
+    #        load_dl1_parameters=True,
+    #        load_simulated=True,
+    #        load_dl2=True,
+    #    )
+    #    return cls(reco_energy_name, geometry_name, gh_score_name, loader, obs_time)
